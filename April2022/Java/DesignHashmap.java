442485f1bc4f1462be3a1a4d6ba99da2c69ea245
Vishwanath Bhat <76242298+Vishwanath2001@users.noreply.github.com>
2022-04-22 13:11:54 +0530

    Update DesignHashmap.java

--- April2022/Java/DesignHashmap.java
+++ April2022/Java/DesignHashmap.java
@@ -36,3 +36,47 @@ class MyHashMap {
  * int param_2 = obj.get(key);
  * obj.remove(key);
  */
+
+
+
+
+// Improving the above solution by increasing the array size when needed
+class MyHashMap {
+    
+    Pair[] arr= null;
+    public MyHashMap() {
+       
+        arr=new Pair[100];
+        
+    }
+    
+    public void put(int key, int value) {
+        if(key>=arr.length){
+            expand(key);
+        }
+        
+        arr[key]=new Pair(key,value);
+    }
+    
+    public int get(int key) {
+        if(key>=arr.length){
+            expand(key);
+        }
+        if((arr[key]==null)){
+            return -1;
+        }
+        Pair<Integer,Integer> temp= arr[key];
+        return temp.getValue();
+    }
+    
+    public void remove(int key) {
+        if(key>=arr.length){
+            expand(key);
+        }
+        arr[key]=null;
+    }
+    
+    private void expand(int key){
+        arr= Arrays.copyOf(arr, key+2);
+    }
+}
